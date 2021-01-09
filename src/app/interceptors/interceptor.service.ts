5fcfb0e18193df0187886770217a6e5a1c2518c7
Jorge Callisaya <sanchezenrique580@gmail.com>
2021-01-08 23:38:10 -0400

    Agregando headers y manejo de errores en el interceptor

--- src/app/interceptors/interceptor.service.ts
+++ src/app/interceptors/interceptor.service.ts
@@ -1,6 +1,7 @@
-import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { catchError } from 'rxjs/operators';
+import { HttpErrorResponse, HttpEvent, HttpHandler, HttpHeaders, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,8 +10,21 @@ export class InterceptorService implements HttpInterceptor{
 
   constructor() { }
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    console.log('Paso por el interceptor');
-    
-    return next.handle(req)
+    const headers = new HttpHeaders({
+      'token-usuario':'ABC124567890ZYX'
+    });
+    const reqClone = req.clone({
+      headers
+    });
+
+    return next.handle(reqClone).pipe(
+      catchError(this.manejarError)
+    )
+  }
+  manejarError(error:HttpErrorResponse){
+    console.log('Sucedío un error');
+    console.log('Registrado en log file');
+    console.warn(error)
+    return throwError('Error personalizado')
   }
 }
--- src/app/services/usuarios.service.ts
+++ src/app/services/usuarios.service.ts
@@ -11,21 +11,13 @@ export class UsuariosService {
 
   obtenerUsuario(){
     const params = new HttpParams().append('page','1');
-    // const headers = new HttpHeaders({
-    //   'token-usuario':'ABC124567890ZYX'
-    // });
-    return this.http.get('https://reqres.in/api/user',{
+
+    return this.http.get('https://reqres123.in/api/user',{
       params,
       // headers
     }).pipe(
       map(resp =>resp['data']),
-      catchError(this.manejarError)
     );
   }
-  manejarError(error:HttpErrorResponse){
-    console.log('Sucedío un error');
-    console.log('Registrado en log file');
-    console.warn(error)
-    return throwError('Error personalizado')
-  }
+  
 }
